15cb814cadd463ee5d18047aea9cec98f473c39e
shakedx <kolesnikov_danil123@mail.ru>
2022-02-14 02:09:49 +0200

    Добавьте файлы проекта.

---
+++ laba1srr/Body.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1srr
+{
+    public class Body
+    {
+        public double a { get; set; } = 0;
+        public double b { get; set; } = 0;
+        public double c { get; set; } = 0;
+
+        public Body(double a, double b, double c, double d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void Max()
+        {
+
+            double max = 4 * (a + b + c);
+
+        }
+        public double Area()
+        {
+            return 2 * (a * b + a * c + b * c);
+        }
+
+
+        public string Print()
+        {
+
+            return $"{(2 * (a * b + a * c + b * c))}\n" +
+                   $"{4 * (a + b + c)}";
+            //Без textd не компилируется.
+            //Как сделать что бы кнопки нажимались и выводилось не одновременно?
+        }
+    }
+}
---
+++ laba1srr/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba1srr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick1(object sender, RoutedEventArgs e)
+        {
+
+            Body body = new Body(double.Parse(texta.Text), double.Parse(textb.Text), double.Parse(textc.Text),
+                double.Parse(textd.Text));
+            Result.Text = body.Print();
+        }
+
+        private void ButtonBase_OnClick2(object sender, RoutedEventArgs e)
+        {
+            Body body = new Body(double.Parse(texta.Text), double.Parse(textb.Text), double.Parse(textc.Text),
+                double.Parse(textd.Text));
+            Result.Text = body.Print();
+        }
+
+
+    }
+}
